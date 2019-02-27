cc25815a48af829e9c6f7134241856cedacc5c57
goonxh <xh011234@163.com>
2019-02-27 22:24:39 +0800

    drag

--- src/components/vue-easy-drag.vue
+++ src/components/vue-easy-drag.vue
@@ -1,33 +1,116 @@
 <template>
-    <div id="box" @click="boxClick">
-        <p>{{text}}</p>
-    </div>
+  <div
+    class="dragBox"
+    :style="style"
+    @mousedown="mouseDown($event)"
+    @mouseup="mouseUp($event)"
+    >
+    <slot></slot>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            text: '',
-        }
+  data() {
+    return {
+      press: false,
+      moveX : 0,
+      moveY: 0,
+      width: this.w,
+      height: this.h,
+      left: this.l,
+      top: this.t,
+      zIndex: this.z,
+      startX: 0,
+      startY: 0,
+      eX: 0,
+      eY: 0,
+    };
+  },
+  props: {
+    w: {
+      type: Number,
+      default: 50
     },
-    created() {
-        
+    h: {
+      type: Number,
+      default: 50
     },
-    methods: {
-        boxClick() {
-            this.text = '恭喜发财！';
-        }
+    t: {
+      type: Number,
+      default: 0
+    },
+    l: {
+      type: Number,
+      default: 0
+    },
+    z: {
+      type: [String, Number],
+      default: "auto"
     }
-}
+  },
+  watch: {
+      moveX() {
+          this.left = this.moveX - this.diffX;
+      },
+      moveY() {
+          this.top = this.moveY - this.diffY;
+      }
+  },
+  computed: {
+    style() {
+      return {
+        width: this.width + "px",
+        height: this.height + "px",
+        top: this.top + "px",
+        left: this.left + "px",
+        zIndex: this.zIndex
+      };
+    },
+    diffX() {
+        return this.startX - this.eX;
+    },
+    diffY() {
+        return this.startY - this.eY;
+    },
+  },
+  created() {
+    document.onmousemove = () => {
+      this.mouseMove()
+    };
+  },
+  methods: {
+    mouseDown(e) {
+      e.target.style.cursor = "move";
+      this.press = true;
+      this.eX = e.target.getBoundingClientRect().left;
+      this.eY = e.target.getBoundingClientRect().top;
+      this.startX = this.getMousePos().x;
+      this.startY = this.getMousePos().y;
+      console.log(this.eX, this.startX)
+    },
+    mouseUp(e) {
+      e.target.style.cursor = "pointer";
+      this.press = false;
+    },
+    mouseMove() {
+      if (this.press) {
+        this.moveX = this.getMousePos().x;
+        this.moveY = this.getMousePos().y;
+        console.log(this.moveX, this.moveY, this.press);
+      }
+    },
+    getMousePos(event) {
+      let e = event || window.event;
+      return {x: e.clientX, y: e.clientY};
+    },
+  }
+};
 </script>
 
 <style lang="scss">
-#box {
-    width: 120px;
-    height: 120px;
-    border-radius: 15px;
-    background-color: #123456;
-    cursor: pointer;
+.dragBox {
+  cursor: pointer;
+  position: absolute;
 }
 </style>
--- src/demo/app.vue
+++ src/demo/app.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        <vue-drag></vue-drag>
+    <div id="app">
+        <vue-easy-drag :w="100" 
+                       :h="100"
+                       :t="250">
+        </vue-easy-drag>
     </div>
 </template>
 
 <script>
-import vueDrag from '../components/vue-easy-drag.vue';
+import vueEasyDrag from '../components/vue-easy-drag.vue';
 export default {
     name: 'app',
     components: {
-        vueDrag,
+        vueEasyDrag,
     },
 }
 </script>
+
+<style lang="scss">
+#app{
+    width: 100vh;
+    height: 98vh;
+    position: relative;
+    .dragBox{
+        border-radius: 50%; 
+        background-color: #123456;
+    }
+}
+</style>
