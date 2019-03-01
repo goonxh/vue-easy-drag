bee01af227d3bb46c97fc6a7917bd439f5f6c845
goonxh <xh011234@163.com>
2019-03-01 11:31:16 +0800

    update demo

--- src/demo/app.vue
+++ src/demo/app.vue
@@ -2,31 +2,34 @@
     <div id="app">
         <vue-easy-drag :w="100"
                        :h="100"
-                       :l="50"
-                       :t="50"
+                       :l="900"
+                       :t="400"
+                       :z="99"
                        :pw="pw"
                        :ph="ph"
                        onselectstart="return false;">
             <div>#1</div>
         </vue-easy-drag>
-        <vue-easy-drag :w="50"
-                       :h="50"
-                       :l="200"
-                       :t="50"
-                       :pw="pw"
-                       :ph="ph"
-                       onselectstart="return false;">
-            <div>#2</div>
-        </vue-easy-drag>
-        <vue-easy-drag :w="50"
-                       :h="50"
-                       :l="200"
-                       :t="250"
-                       :pw="pw"
-                       :ph="ph"
-                       onselectstart="return false;">
-            <div>#3</div>
-        </vue-easy-drag>
+        <div class="dragContainer">
+            <vue-easy-drag :w="50"
+                           :h="50"
+                           :l="200"
+                           :t="50"
+                           :pw="pw"
+                           :ph="ph"
+                           onselectstart="return false;">
+                <div>#2</div>
+            </vue-easy-drag>
+            <vue-easy-drag :w="50"
+                           :h="50"
+                           :l="200"
+                           :t="250"
+                           :pw="pw"
+                           :ph="ph"
+                           onselectstart="return false;">
+                <div>#3</div>
+            </vue-easy-drag>
+        </div>
     </div>
 </template>
 
@@ -53,17 +56,26 @@
 </script>
 
 <style lang="scss">
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    html, body {
+        width: 100%;
+        height: 100%;
+    }
     #app {
-        width: 50vw;
-        height: 50vh;
-        border: 1px solid #111;
+        width: 100%;
+        height: 100%;
         position: relative;
-        padding: 20px;
-        margin: 50px;
-
+        .dragContainer {
+            width: 800px;
+            height: 400px;
+            border: 1px solid #111;
+            position: relative;
+            padding: 20px;
+        }
         .dragBox {
-            width: 100px;
-            height: 100px;
             border-radius: 50%;
             background-color: #123456;
             div{
